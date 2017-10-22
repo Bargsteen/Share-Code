29a316257f22568b5a37d241455f93f8818a71fb
Kasper Dissing Bargsteen <kasper@bargsteen.com>
2017-10-22 08:27:50 +0200

    Updated variable names to follow PEP8

--- get.py
+++ get.py
@@ -1,25 +1,30 @@
 import os
 import sys
 import requests
-filename = sys.argv[1] 
+
+file_name = sys.argv[1] 
 password = str(raw_input("Enter the password for the file"))
+
 if (password == ""):
     password = "null"
-r = requests.post("http://sharecode.co.nf/getcode.php?filename="+filename+"&password="+password+"&getname=true")
-localname = r.text
-r = requests.post("http://sharecode.co.nf/getcode.php?filename="+filename+"&password="+password)
+
+r = requests.post("http://sharecode.co.nf/getcode.php?filename="+file_name+"&password="+password+"&getname=true")
+local_name = r.text
+
+r = requests.post("http://sharecode.co.nf/getcode.php?filename="+file_name+"&password="+password)
 if (r.text == "False"):
     print "Wrong combination of filename and password"
     exit(0)
-flag = True
+
 for i in os.listdir(os.getcwd()):
-	if (i == localname):
-		print "A file with the name " + localname + " already exists"
-		newname = str(raw_input("Enter new file name "))
-		if (newname != ""):
-			localname = newname
+	if (i == local_name):
+		print "A file with the name " + local_name + " already exists"
+		new_name = str(raw_input("Enter new file name "))
+		if (new_name != ""):
+			local_name = new_name
 		break
-file = open(localname,'w+')
+		
+file = open(local_name,'w+')
 file.write(r.text)
 file.close()
 
