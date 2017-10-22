04509603f3e0146b5e6f2a21a525dd012a47ab66
Kasper Dissing Bargsteen <kasper@bargsteen.com>
2017-10-22 08:22:31 +0200

    Improved clarity in naming and text feedback

--- send.py
+++ send.py
@@ -1,19 +1,27 @@
 import os
 import sys
 import requests
+
 filename = sys.argv[1]
-flag = False
+
+file_found = False
+
 for i in (os.listdir(os.getcwd())):
     if (i == filename):
-        flag = True
-if (not flag):
-    print filename + " file not found"
+        file_found = True
+        
+if (not file_found):
+    print "The file '" + filename + "' could not be found."
     exit(0)
-falg = False
-files = {'file': open(filename, 'rb')}
-password = str(raw_input("Enter password for your file "))
+    
+file = {'file': open(filename, 'rb')}
+
+password = str(raw_input("Enter password for protection, or press 'Enter' to skip:"))
+
 if (password == ""):
     password = "null"
+    
 r = requests.post("http://sharecode.co.nf/server.php?filename="+filename+"&password="+password,files=files)
+
 print r.text
 
